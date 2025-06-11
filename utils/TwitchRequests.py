639e4d73b74e0027cd64422da1d1d72439c7463e
oxsolsticexo <carlos_007_34@outlook.com>
2025-06-11 20:48:03 +0200

    update environ

--- utils/TwitchRequests.py
+++ utils/TwitchRequests.py
@@ -7,9 +7,9 @@ class TwitchRequests:
     
     def code_to_token(self,code: str):
         params = {
-            "client_id":os.getenv("CLIENT_ID"),
-            "client_secret":os.getenv("CLIENT_SECRET"),
-            "redirect_uri":os.getenv("REDIRECT_URI"),
+            "client_id":os.environ["CLIENT_ID"],
+            "client_secret":os.environ["CLIENT_SECRET"],
+            "redirect_uri":os.environ["REDIRECT_URI"],
             "code":code,
             "grant_type":"authorization_code"
         }
@@ -17,7 +17,7 @@ class TwitchRequests:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
         }
-        response = requests.post(os.getenv("GET_TOKEN_URL"),data=params,headers=headers)
+        response = requests.post(os.environ["GET_TOKEN_URL"],data=params,headers=headers)
         if response.status_code == 200:
             data = response.json()
             access_token = data["access_token"]
